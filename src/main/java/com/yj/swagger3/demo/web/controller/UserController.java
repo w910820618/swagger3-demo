dcedc7533a5eaa95291953a89f132d0b44037f5f
wuzhouyang <w910820618@163.com>
2023-12-21 21:18:55 +0800

    修改完成数据结构

--- src/main/java/com/yj/swagger3/demo/web/controller/UserController.java
+++ src/main/java/com/yj/swagger3/demo/web/controller/UserController.java
@@ -2,8 +2,10 @@ package com.yj.swagger3.demo.web.controller;
 
 
 import com.yj.swagger3.demo.web.domain.User;
+import com.yj.swagger3.demo.web.service.UserService;
 import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,74 +25,70 @@ import java.util.*;
 @RequestMapping("/user")
 public class UserController {
 
-    static List<User> list = new ArrayList<>();
-    static int maxLimit = 20;
+    @Autowired
+    private UserService userService;
+
+    // init user list
+    private static List<User> userList = new ArrayList<>();
 
     static {
 
-        for (int i = 0; i < maxLimit; i++) {
+        for (int i = 0; i < 20; i++) {
             User u = new User();
             u.setId(i);
             u.setUsername(UUID.randomUUID().toString());
             u.setAddress("address-" + i);
             u.setTelephone("1001011-" + i);
-            list.add(u);
-
+            userList.add(u);
         }
     }
 
-    /**
-     * Get a list of user information
-     *
-     * @return
-     */
-    @ApiOperation(value = "Get a list of users", notes = "Get all user information", tags = {"Get a list of user information tags"}, response = List.class)
-    @GetMapping("/getUserList")
+    // create user list
+    @ApiOperation(value = "Create user list", notes = "Create user list")
+    @GetMapping("/createList")
+    public void createList() {
+        for (User user : userList) {
+            userService.addUser(user);
+        }
+    }
+
+
+    // create user
+    @ApiOperation(value = "Create user", notes = "Create user")
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        userService.addUser(user);
+        return user;
+    }
+
+    // get user by id
+    @ApiOperation(value = "Get user by id", notes = "Get user by id")
+    @GetMapping("/get/{id}")
+    public User getUserById(@PathVariable("id") Integer id) {
+        return userService.selectByPrimaryKey(id);
+    }
+
+    // get user list
+    @ApiOperation(value = "Get user list", notes = "Get user list")
+    @GetMapping("/list")
     public List<User> getUserList() {
-
-        return list;
+        return userService.selectAll();
     }
 
-    /**
-     * Obtain user information based on user ID and address
-     *
-     * @param userId
-     * @param address
-     * @return
-     */
-    @ApiOperation(value = "API for obtaining user information", notes = "Get all user information - a single input parameter", tags = {"API for obtaining user information tags"}, response = User.class)
-    @GetMapping("/getUser")
-    public User getUser(@ApiParam(name = "userId", value = "User Id", required = true) Long userId, @ApiParam(name = "address", value = "User Address", required = true) String address) {
-
-        return list.stream().filter(u -> u.getId().equals(userId) && u.getAddress().equals(address)).filter(Objects::nonNull).findFirst().orElse(new User());
+    // update user
+    @ApiOperation(value = "Update user", notes = "Update user")
+    @PutMapping("/update")
+    public User updateUser(@RequestBody User user) {
+        userService.updateByPrimaryKey(user);
+        return user;
     }
 
-    /**
-     * Obtain user information based on user ID and address
-     *
-     * @param userVO
-     * @return
-     */
-    @ApiOperation(value = "API 2 for obtaining user information", notes = "Obtain User Information API 2 - Entity Object Input Parameter", tags = {"API 2 for obtaining user information tags"}, response = User.class)
-    @PostMapping("/getUser2")
-    public User getUser2(@RequestBody UserVO userVO) {
-
-        return list.stream().filter(u -> u.getId().equals(userVO.getId()) && u.getAddress().equals(userVO.getAddress())).filter(Objects::nonNull).findFirst().orElse(new User());
-    }
-
-    /**
-     * Delete User
-     *
-     * @param userId
-     * @return
-     */
-    @ApiOperation(value = "Delete User Interface", notes = "Delete the userId parameter of the user interface")
-    @ApiImplicitParams({@ApiImplicitParam(name = "userId", value = "User Id", required = true),})
-    @PostMapping("/deleteUser")
-    public String deleteUser(Long userId) {
-
-        list.stream().filter(u -> u.getId().equals(userId)).filter(Objects::isNull).findFirst().ifPresent(u1 -> list.removeIf(Objects::isNull));
-
+    // delete user by id
+    @ApiOperation(value = "Delete user by id", notes = "Delete user by id")
+    @DeleteMapping("/delete/{id}")
+    public String deleteUserById(@PathVariable("id") Integer id) {
+        userService.deleteByPrimaryKey(id);
         return "success";
     }
+
 }
